3a2332269e0ee813b020017173216ffb35e163f6
Flavio Benedini <flavio.benedini@gmail.com>
2022-05-04 21:08:13 -0300

    fix: ajustando o caminho da view no controller

--- 04-sistema-de-roteamento-02/Controllers/seriesController.js
+++ 04-sistema-de-roteamento-02/Controllers/seriesController.js
@@ -4,7 +4,7 @@ const path = require("path")
 const controller = {
   index: (req, res) => res.send(series),
   porGenero: (req, res) => res.send(series.filter(serie => serie.genero.toLowerCase() === req.params.genero.toLowerCase())),
-  criar: (req, res) => res.sendFile(path.join(__dirname,"..", "view", "criar-serie.html"))
+  criar: (req, res) => res.sendFile(path.join(__dirname,"..", "views", "criar-serie.html"))
 }
 
 module.exports = controller
